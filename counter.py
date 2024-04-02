b131be0d290d9b245cbadeb51736f23987312992
Salman <154886467+Boogeyman-19@users.noreply.github.com>
2024-04-02 23:00:43 +0530

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,25 @@
+# program to count the number of words in a user given text
+
+# Step-1: introduction for the program in heading 
+
+print("\n\n**************** WELCOME TO WORD COUNT PROGRAM***************")
+print("---------------------------------------------------------------\n")
+
+# Step-2: Take the input from the user either its a sentence or a paragraph
+
+sentence=input("Enter your text to check for count of words :\n \n")
+print("\n your entered text is :\n\n\t",sentence)
+
+# Step-3: Convert your text into list using split() function
+
+list=sentence.split()
+
+# Step-4: find the length of the list using len() function
+
+length=len(list)
+
+# Step-5: This is the final step print the length of the list
+
+print(f"\n The total number of words in the given text is: { length}") 
+print()
+print()
